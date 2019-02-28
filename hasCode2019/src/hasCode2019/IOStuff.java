1c69ec3123215490fc9364c9a224e9dc749794dc
Samuel Beck <sammy_13@gmx.net>
2019-02-28 20:08:38 +0100

    Add input reader

--- hasCode2019/src/hasCode2019/IOStuff.java
+++ hasCode2019/src/hasCode2019/IOStuff.java
@@ -20,8 +20,49 @@ public class IOStuff {
 		}
 		return readAllLines;
 	}
-	
-	public void writeOut() {
 
+    public static List<Image> readFile(Path path) {
+        List<String> lines = new ArrayList<String>();
+        List<Image> images = new ArrayList<Image>();
+
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // Read number of images
+        int numImages = Integer.parseInt(lines.remove(0));
+
+        // Iterate over lines to read images
+        for (String line : lines) {
+
+            String[] components = line.split(" ");
+
+            // Determine orientation
+            Orientation orientation;
+            if (components[0].equals("H")) {
+                orientation = Orientation.Horizontal;
+            } else {
+                orientation = Orientation.Veritcal;
+            }
+
+            // Determine num of tags
+            int numTags = Integer.parseInt(components[1]);
+
+            // Determine tags
+            ArrayList<String> tags = new ArrayList<String>();
+            for (int i = 2; i < components.length; i++) {
+                tags.add(components[i]);
+            }
+
+            images.add(new Image(orientation, tags, numTags));
+        }
+
+        if (images.isEmpty()) {
+            System.exit(1);
+        }
+        return images;
 	}
 }
--- hasCode2019/src/hasCode2019/mainClass.java
+++ hasCode2019/src/hasCode2019/mainClass.java
@@ -1,33 +1,67 @@
 package hasCode2019;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class mainClass {
 
-	public static void main(String[] args) {
-		System.out.println("standart syso");
-		System.out.println(IOStuff.loadMainInputFile());
-		
-		
-		
-		ArrayList<Image> ioImages = new ArrayList<>();
-		ArrayList<Image> verticalImages = new ArrayList<Image>();
-		
-		
+    static List<Image> images;
 
-		for (Image image : ioImages) {
-			if(image.getOrientation().equals(Orientation.Veritcal)) {			
-				verticalImages.add(image);
-			}
-		}
-		 
-		Collections.sort(verticalImages);
-		//
-		
-		
-		
+    private static void readImages() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Which input file do you want to read? (a, b, c, d, e)");
+        try {
+            String input = br.readLine();
+
+            if (!input.equals("a") && !input.equals("b") && !input.equals("c") && !input.equals("d") && !input.equals("e")) {
+                System.out.println("Invalid input");
+                System.exit(1);
+            }
+
+            Path path = null;
+
+            switch (input) {
+                case "a":
+                    path = Paths.get("input/a_example.txt");
+                    break;
+                case "b":
+                    path = Paths.get("input/b_lovely_landscapes.txt");
+                    break;
+                case "c":
+                    path = Paths.get("input/c_memorable_moments.txt");
+                    break;
+                case "d":
+                    path = Paths.get("input/d_pet_pictures.txt");
+                    break;
+                case "e":
+                    path = Paths.get("input/e_shiny_selfies.txt");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    System.exit(1);
+            }
+
+            // Read images from input file
+            images = IOStuff.readFile(path);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Couldn't close buffered reader");
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+        // Read images from input file and save them in image list
+        readImages();
 	}
 
 }
